3d8b971148d0d66c7c01ef9eb72d840bfef3553e
THRBY <qwerty1236547890>
2019-06-26 01:47:48 +0600

    add dialog text

--- HDBK_BOT.py
+++ HDBK_BOT.py
@@ -8,6 +8,16 @@ bot = telebot.TeleBot(TOKEN)
 def start_handler(message):
     bot.send_message(message.chat.id, 'Hi!')
 
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    if text == 'привет':
+        bot.send_message(chat_id, 'Привет, я бот - парсер хабра.')
+    elif text == 'как дела?':
+        bot.send_message(chat_id, 'Лушче всех!')
+    else:
+        bot.send_message(chat_id, 'Шо?')
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
