cc090d09aa4bb68e7a4823dbd911c669b01130c8
THRBY <darkotherwise@gmail.com>
2019-07-12 11:06:54 +0600

    Add files via upload

---
+++ githubAPI.py
@@ -0,0 +1,28 @@
+import requests
+
+#url
+py_url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+java_url = 'https://api.github.com/search/repositories?q=language:java&sort=stars'
+
+#python#
+py_r = requests.get(py_url)
+py_response_dict = py_r.json()
+py_repo_dicts = py_response_dict['items']
+
+
+py_names, py_repositorys, py_desriptions = [], [], []
+for py_repo_dict in py_repo_dicts:
+        py_names.append(py_repo_dict['name'])
+        py_repositorys.append(py_repo_dict['html_url'])
+        py_desriptions.append(py_repo_dict['description'])
+
+#java#
+java_r = requests.get(java_url)
+java_response_dict =java_r.json()
+java_repo_dicts = java_response_dict['items']
+
+java_names, java_repository, java_desriptions = [], [], []
+for java_repo_dict in java_repo_dicts:
+        java_names.append(java_repo_dict['name'])
+        java_repository.append(java_repo_dict['html_url'])
+        java_desriptions.append(java_repo_dict['description'])
