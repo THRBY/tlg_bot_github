a3855f50e7bb4cc50bb30a4e0c5caeac2b7362ff
THRBY <darkotherwise@gmail.com>
2019-07-12 11:06:38 +0600

    Add files via upload

--- githubBot.py
+++ githubBot.py
@@ -1,36 +1,9 @@
 import telebot
 import requests
-
-py_url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
-java_url = 'https://api.github.com/search/repositories?q=language:java&sort=stars'
-
-#python#
-py_r = requests.get(py_url)
-# Сохранение ответа API в переменной.
-py_response_dict = py_r.json()
-# Анализ информации о репозиториях.
-py_repo_dicts = py_response_dict['items']
-# Анализ первого репозитория.
-
-py_names, py_repositorys, py_desriptions = [], [], []
-for py_repo_dict in py_repo_dicts:
-        py_names.append(py_repo_dict['name'])
-        py_repositorys.append(py_repo_dict['html_url'])
-        py_desriptions.append(py_repo_dict['description'])
-
-#java#
-java_r = requests.get(java_url)
-java_response_dict =java_r.json()
-java_repo_dicts = java_response_dict['items']
-
-java_names, java_repository, java_desriptions = [], [], []
-for java_repo_dict in java_repo_dicts:
-        java_names.append(java_repo_dict['name'])
-        java_repository.append(java_repo_dict['html_url'])
-        java_desriptions.append(java_repo_dict['description'])
+import githubAPI
 
 # Bot tlg###
-bot = telebot.TeleBot('') #@Mobbius_Bot
+bot = telebot.TeleBot('512500856:AAEq_uMUB5omgDPdjuYTGLHw88JphT8TnfA') #@Mobbius_Bot
 
 @bot.message_handler(commands=['start','help'])
 def send_welcome(message):
@@ -52,15 +25,16 @@ def send_py(message):
         text = message.text.lower()
         #print(text)
         for i in range(4):
-                #bot.reply_to(message, names[i])
-                bot.send_message(chat_id, py_repositorys[i])
+                bot.send_message(chat_id, githubAPI.py_desriptions[i])
+                bot.send_message(chat_id, githubAPI.py_repositorys[i])
 
 #@bot.message_handler(commands=['java'])
 def send_java(message):
         chat_id = message.chat.id
         text = message.text.lower()
         for i in range(3):
-                bot.send_message(chat_id, java_repository[i])        
+                bot.send_message(chat_id, githubAPI.java_desriptions[i])
+                bot.send_message(chat_id, githubAPI.java_repository[i])        
 
 
 if __name__ == '__main__':
