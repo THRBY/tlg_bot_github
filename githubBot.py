254c09005dbfb66c78f696cbf4c48fd1b3555288
THRBY <darkotherwise@gmail.com>
2019-11-04 03:10:58 +0600

    fix output message

--- githubBot.py
+++ githubBot.py
@@ -7,14 +7,17 @@ bot = telebot.TeleBot('512500856:AAEq_uMUB5omgDPdjuYTGLHw88JphT8TnfA') #@Mobbius
 
 @bot.message_handler(commands=['start','help'])
 def send_welcome(message):
+        text = 'python'
         bot.send_message(message.chat.id, 'Hi!\nВыберите нужны раздел (такой фичи пока нет...)')
-        
+        bot.register_next_step_handler(message, send_py)
+
+                
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): 
         #chat_id = message.chat.id
         text = message.text.lower()
-        if text == 'python':
+        if text == 'python1':
                 bot.register_next_step_handler(message, send_py)
         if text == 'java':
                 bot.register_next_step_handler(message, send_java)
@@ -22,19 +25,18 @@ def repeat_all_messages(message):
 #@bot.message_handler(commands=['python'])
 def send_py(message):
         chat_id = message.chat.id
-        text = message.text.lower()
-        #print(text)
-        for i in range(4):
-                bot.send_message(chat_id, githubAPI.py_desriptions[i])
-                bot.send_message(chat_id, githubAPI.py_repositorys[i])
+        #text = message.text.lower()
+        
+        for i in range(3):
+                bot.send_message(chat_id, githubAPI.py_names[i] + '\n\n' + githubAPI.py_repositorys[i])                    
+        
 
 #@bot.message_handler(commands=['java'])
 def send_java(message):
         chat_id = message.chat.id
-        text = message.text.lower()
+        #text = message.text.lower()
         for i in range(3):
-                bot.send_message(chat_id, githubAPI.java_desriptions[i])
-                bot.send_message(chat_id, githubAPI.java_repository[i])        
+                bot.send_message(chat_id, githubAPI.java_names[i] + '\n\n' + githubAPI.java_repository[i])
 
 
 if __name__ == '__main__':
