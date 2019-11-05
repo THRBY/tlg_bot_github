15660a21e35b5a4325d1b8c371e5ff764937455a
THRBY <darkotherwise@gmail.com>
2019-11-06 04:05:37 +0600

    change code, remote def send of python and java. Add loop in def repeat_all messages

--- githubBot.py
+++ githubBot.py
@@ -7,36 +7,26 @@ bot = telebot.TeleBot('512500856:AAEq_uMUB5omgDPdjuYTGLHw88JphT8TnfA') #@Mobbius
 
 @bot.message_handler(commands=['start','help'])
 def send_welcome(message):
-        text = 'python'
+        #text = message.text.lower()
         bot.send_message(message.chat.id, 'Hi!\nВыберите нужны раздел (такой фичи пока нет...)')
-        bot.register_next_step_handler(message, send_py)
-
+        bot.register_next_step_handler(message, repeat_all_messages)
                 
 
-@bot.message_handler(content_types=["text"])
+#@bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): 
-        #chat_id = message.chat.id
+        chat_id = message.chat.id
         text = message.text.lower()
-        if text == 'python1':
-                bot.register_next_step_handler(message, send_py)
+        if text == 'python':
+                #bot.register_next_step_handler(message, send_py)
+                for i in range(3):
+                        bot.send_message(chat_id, githubAPI.py_names[i] + '\n\n' + githubAPI.py_repositorys[i])
         if text == 'java':
-                bot.register_next_step_handler(message, send_java)
+                #bot.register_next_step_handler(message, send_java)
+                for i in range(3):
+                        bot.send_message(chat_id, githubAPI.java_names[i] + '\n\n' + githubAPI.java_repository[i])
+        else:
+                bot.reply_to(message, 'oooops')
 
-#@bot.message_handler(commands=['python'])
-def send_py(message):
-        chat_id = message.chat.id
-        #text = message.text.lower()
-        
-        for i in range(3):
-                bot.send_message(chat_id, githubAPI.py_names[i] + '\n\n' + githubAPI.py_repositorys[i])                    
-        
-
-#@bot.message_handler(commands=['java'])
-def send_java(message):
-        chat_id = message.chat.id
-        #text = message.text.lower()
-        for i in range(3):
-                bot.send_message(chat_id, githubAPI.java_names[i] + '\n\n' + githubAPI.java_repository[i])
 
 
 if __name__ == '__main__':
